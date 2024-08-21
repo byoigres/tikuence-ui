f393829adea5d1e3b78e87c1b1b3607f41efdd72
Sergio Flores <byoigres@gmail.com>
2024-08-20 20:37:13 -0600

    Create CompleteProfile page.

---
+++ src/Pages/Auth/CompleteProfile.jsx
@@ -0,0 +1,163 @@
+
+import React, { useState } from "react";
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+import TextField from '@mui/material/TextField';
+import FormControl from '@mui/material/FormControl';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import FormGroup from '@mui/material/FormGroup';
+import Checkbox from '@mui/material/Checkbox';
+import Button from '@mui/material/Button';
+import Link from '@mui/material/Link';
+import MainLayout from "../../components/layouts/MainLayout";
+import { Link as InertiaLink } from "@inertiajs/react";
+import { styled } from '@mui/material/styles';
+
+const StyledForm = styled('form')(() => ({
+  '& .MuiFormControl-root, & .MuiButton-root': {
+    marginTop: '1rem',
+  },
+  marginBottom: '1rem',
+}));
+
+const LoginPage = ({ email, name, token, isInvalid = false, isExpired = false }) => {
+  const [values, setValues] = useState({
+    name,
+    username: '',
+    bio: '',
+    tiktokUsername: '',
+    terms: false,
+    token,
+  });
+
+  function handleInputChange(e) {
+    const key = e.target.name;
+    const value = e.target.type === 'checkbox' ? e.target.checked : e.target.value;
+
+    setValues((v) => ({
+      ...v,
+      [key]: value,
+    }));
+  }
+
+  return (
+    <Container maxWidth="sm">
+      {isInvalid && (
+        <Typography variant="h6" color="error" align="center">
+          This link is invalid
+        </Typography>
+      )}
+      {isExpired && (
+        <Typography variant="h6" color="error" align="center">
+          This link has expired
+        </Typography>
+      )}
+      {!isInvalid && !isExpired && (
+        <>
+          <Typography variant="h4" color="primary" align="center" gutterBottom>
+            Complete your profile
+          </Typography>
+          <Typography variant="subtitle2" color="textSecondary" gutterBottom>
+            You are setting your account using your Google account.
+          </Typography>
+          <StyledForm autoComplete="off" autoCorrect="off">
+            <TextField
+              name="email"
+              label="Email"
+              value={email}
+              fullWidth
+              disabled
+              variant="outlined"
+            />
+            <TextField
+              name="name"
+              label="Name"
+              value={values.name}
+              autoComplete="off"
+              onChange={handleInputChange}
+              fullWidth
+              autoFocus
+              variant="outlined"
+            />
+            <TextField
+              name="username"
+              label="Username"
+              value={values.username}
+              autoComplete="off"
+              onChange={handleInputChange}
+              fullWidth
+              variant="outlined"
+            />
+            <TextField
+              name="bio"
+              label="Bio"
+              placeholder="Write a little bit of yourself (optional)"
+              value={values.bio}
+              autoComplete="off"
+              multiline
+              rows={4}
+              onChange={handleInputChange}
+              fullWidth
+              variant="outlined"
+            />
+            <TextField
+              name="tiktokUsername"
+              label="TikTok username"
+              placeholder="(Optional)"
+              value={values.tiktokUsername}
+              autoComplete="off"
+              onChange={handleInputChange}
+              onKeyPress={(e) => {
+                if (!/^([A-Za-z0-9_.])$/.test(e.key)) {
+                  e.preventDefault();
+                }
+              }}
+              fullWidth
+              variant="outlined"
+            />
+            <FormControl required error component="fieldset">
+              <FormGroup>
+                <FormControlLabel
+                  control={
+                    <Checkbox
+                      name="terms"
+                      checked={values.terms}
+                      value={values.terms}
+                      onChange={handleInputChange}
+                      color="primary"
+                    />
+                  }
+                  label={
+                    <>
+                      {`Accept the `}
+                      <Link component={InertiaLink} href="/policies/terms">
+                        terms of service
+                      </Link>
+                      {` and `}
+                      <Link component={InertiaLink} href="/policies/privacy">
+                        privacy policy
+                      </Link>
+                      .
+                    </>
+                  }
+                />
+              </FormGroup>
+            </FormControl>
+            <Button
+              variant="contained"
+              color="primary"
+              disabled={!values.terms}
+              fullWidth
+            >
+              Complete profile
+            </Button>
+          </StyledForm>
+        </>
+      )}
+    </Container>
+  );
+}
+
+LoginPage.layout = (page) => <MainLayout children={page} />;
+
+export default LoginPage;
