481f0da4ec49e70550e9fac3eb4709997c2aed45
Sergio Flores <byoigres@gmail.com>
2024-08-26 20:58:48 -0600

    Add SnackBar provider.
    - Install notistack dependency.
    - Create SnackbarProvider component.
    - Snackbar added to MainLayout.

---
+++ src/components/SnackbarProvider.jsx
@@ -0,0 +1,59 @@
+import React, { useEffect } from 'react';
+import Button from '@mui/material/Button';
+import { SnackbarProvider as NativeSnackbarProvider } from 'notistack';
+
+const SnackBarProvider = ({ children, flash }) => {
+  const notistackRef = React.createRef();
+
+  useEffect(() => {
+    if (flash) {
+      let message = null;
+      let variant = null;
+      const timestamp = localStorage.getItem('_tklastflashtime');
+
+      if (flash.success) {
+        message = flash.success;
+        variant = 'success';
+      } else if (flash.info) {
+        message = flash.info;
+        variant = 'info';
+      } else if (flash.warning) {
+        message = flash.warning;
+        variant = 'warning';
+      } else if (flash.error) {
+        message = flash.error;
+        variant = 'error';
+      }
+
+      if (message && timestamp !== flash.timestamp) {
+        notistackRef.current.enqueueSnackbar(message, {
+          variant,
+        });
+
+        localStorage.setItem('_tklastflashtime', flash.timestamp);
+      }
+    }
+  }, [flash]);
+
+  return (
+    <NativeSnackbarProvider
+      ref={notistackRef}
+      anchorOrigin={{
+        vertical: 'bottom',
+        horizontal: 'center',
+      }}
+      action={(key) => (
+        <Button
+          sx={{ color: 'white' }}
+          onClick={() => notistackRef.current.closeSnackbar(key)}
+        >
+          Dismiss
+        </Button>
+      )}
+    >
+      {children}
+    </NativeSnackbarProvider>
+  );
+};
+
+export default SnackBarProvider;
--- src/components/layouts/MainLayout.jsx
+++ src/components/layouts/MainLayout.jsx
@@ -4,28 +4,32 @@ import { usePage } from "@inertiajs/react";
 import Navbar from "../Navbar";
 import Navigation from "../Navbar/Navigation";
 import { Container } from "@mui/material";
+import SnackbarProvider from "../SnackbarProvider";
 
 const MainLayout = ({ children }) => {
   const {
     props: {
       auth: { isAuthenticated, profile },
+      flash,
     },
   } = usePage();
   return (
     <>
       <CssBaseline />
-      <Navigation>
-        <Navbar isAuthenticated={isAuthenticated} profile={profile} />
-      </Navigation>
-      <Container sx={{ marginTop: 10 }}>
-        {React.Children.map(children, (child) => {
-          if (!React.isValidElement(child)) {
-            return null;
-          }
+      <SnackbarProvider flash={flash}>
+        <Navigation>
+          <Navbar isAuthenticated={isAuthenticated} profile={profile} />
+        </Navigation>
+        <Container sx={{ marginTop: 10 }}>
+          {React.Children.map(children, (child) => {
+            if (!React.isValidElement(child)) {
+              return null;
+            }
 
-          return React.cloneElement(child);
-        })}
-      </Container>
+            return React.cloneElement(child);
+          })}
+        </Container>
+      </SnackbarProvider>
     </>
   );
 };
